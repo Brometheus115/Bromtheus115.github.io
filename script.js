713264b64d691baaea2e533441b8754c30deb557
Brometheus115 <robert359960@hotmail.de>
2024-09-03 13:22:45 +0200

    added all files

---
+++ script.js
@@ -0,0 +1,24 @@
+//console.log("Hey there, this is my first JS File!");
+//print to console
+//window.alert("Hey Good Morning");
+//print to browser window
+//let, const
+//let my_name = "Robert";
+//let age = "28";
+//console.log("Hi, my name is " + my_name);
+//console.log("I am " + age + " years old");
+let myClassorIDname = document.getElementsByID("ClassorIDname")
+console.log("found this element by id");
+console.log(ClassorIDname)
+
+let allParagraphs = document.getElementsbyByTagName("p");
+console.log("all Paragraph Elements");
+console.log(allParagraphs);
+
+let allHeaders = document.getElementsbyByTagName("h1");
+console.log("all header elements");
+console.log(allHeaders);
+
+let allclassname = document.getElementsbyByClassName("classname");
+console.log("all class names Elements");
+console.log(allclassname);
